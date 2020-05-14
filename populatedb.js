157f905eff8118ca6fdf813e9c4b8e3ec419c823
Julio Bermudez <jbermudezfc90@gmail.com>
2020-05-14 14:12:07 -0400

    populated db with script

--- populatedb.js
+++ populatedb.js
@@ -211,24 +211,6 @@ function laptopCreate(name, description, category, price, stock, cb) {
     });
 }
 
-function landlinePhoneCreate(name, cb) {
-    const brand = new Brand({
-        name,
-    });
-
-    brand.save(function (err) {
-        if (err) {
-            cb(err, null);
-            return;
-        }
-        console.log('New Brand: ' + brand);
-        brands.push(brand);
-        cb(null, brand);
-    });
-}
-
-//CREATE MODEMS!!!
-
 function createCategories(cb) {
     async.series(
         [
@@ -266,7 +248,7 @@ function createModems(cb) {
                 modemCreate(
                     'MODEM AMAZING',
                     'This modem is very good, very fast, and small',
-                    'category',
+                    categories[1],
                     25,
                     7,
                     callback,
@@ -276,7 +258,7 @@ function createModems(cb) {
                 modemCreate(
                     'ROUTER AWESOME',
                     'Very efficient router, it does everything it says and more!',
-                    'category',
+                    categories[1],
                     30,
                     16,
                     callback,
@@ -286,7 +268,7 @@ function createModems(cb) {
                 modemCreate(
                     'MODEM SPECTACULAR',
                     'Despite its name, this modem is not very spectacular',
-                    'category',
+                    categories[1],
                     16,
                     72,
                     callback,
@@ -312,7 +294,7 @@ function createSmartphones(cb) {
                 smartphoneCreate(
                     'Ultra One Stellar 5G',
                     { OS: 'Android', RAM: 8, ROM: 128, battery: 5000, cameras },
-                    'category',
+                    categories[2],
                     2500,
                     240,
                     callback,
@@ -327,7 +309,7 @@ function createSmartphones(cb) {
                 smartphoneCreate(
                     'Increndible Smartphone',
                     { OS: 'Android', RAM: 16, ROM: 256, battery: 7000, cameras },
-                    'category',
+                    categories[2],
                     3000,
                     2,
                     callback,
@@ -340,7 +322,7 @@ function createSmartphones(cb) {
                 smartphoneCreate(
                     'MODEM SPECTACULAR',
                     { OS: 'Android', RAM: 2, ROM: 16, battery: 3000, cameras },
-                    'category',
+                    categories[2],
                     700,
                     24,
                     callback,
@@ -359,7 +341,7 @@ function createLandlinePhones(cb) {
                 landlinePhoneCreate(
                     'Imaginary',
                     'Extremely realistic and touch sensitive phone',
-                    'category',
+                    categories[2],
                     39,
                     0,
                     callback,
@@ -378,7 +360,7 @@ function createKeyboards(cb) {
                 keyboardCreate(
                     'Imaginary',
                     'Extremely realistic and touch sensitive phone',
-                    'category',
+                    categories[0],
                     39,
                     0,
                     callback,
@@ -388,7 +370,7 @@ function createKeyboards(cb) {
                 keyboardCreate(
                     'Astonishingly Wireless',
                     'This wireless phone will allow you to call your mom from anywhere',
-                    'category',
+                    categories[0],
                     12,
                     34,
                     callback,
@@ -407,7 +389,7 @@ function createMice(cb) {
                 mouseCreate(
                     'Buttonful',
                     `You won't need a keyboard along this mouse thanks to its 136 buttons perfect for any situation`,
-                    'category',
+                    categories[0],
                     72,
                     42,
                     callback,
@@ -417,7 +399,7 @@ function createMice(cb) {
                 mouseCreate(
                     'Buttonless',
                     'A mouse entirely made of a screen that is extremely responsive to your fingers',
-                    'category',
+                    categories[0],
                     340,
                     6,
                     callback,
@@ -436,7 +418,7 @@ function createTelevisions(cb) {
                 televisionCreate(
                     'Humungous',
                     'A 102 inches OLED panel that will immerse you for the rest of your life',
-                    'category',
+                    categories[1],
                     4341,
                     2,
                     callback,
@@ -459,7 +441,7 @@ function createLaptops(cb) {
                     CPU: 'Stellar 764',
                     GPU: 'Electric 122',
                 };
-                laptopCreate('XMWD Laptop', description, 'category', 768, 53, callback);
+                laptopCreate('XMWD Laptop', description, categories[1], 768, 53, callback);
             },
             function (callback) {
                 const description = {
@@ -469,7 +451,7 @@ function createLaptops(cb) {
                     CPU: 'Ghostly 340',
                     GPU: 'Black 78x',
                 };
-                laptopCreate('30th Gen Laptop', description, 'category', 120, 435, callback);
+                laptopCreate('30th Gen Laptop', description, categories[1], 120, 435, callback);
             },
             function (callback) {
                 const description = {
@@ -479,86 +461,7 @@ function createLaptops(cb) {
                     CPU: 'Yeet 3470',
                     GPU: 'Liquid 90x',
                 };
-                laptopCreate('NwoL Laptop', description, 'category', 1999, 27, callback);
-            },
-        ],
-        // optional callback
-        cb,
-    );
-}
-
-function createBooks(cb) {
-    async.parallel(
-        [
-            function (callback) {
-                bookCreate(
-                    'The Name of the Wind (The Kingkiller Chronicle, #1)',
-                    'I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.',
-                    '9781473211896',
-                    authors[0],
-                    [genres[0]],
-                    callback,
-                );
-            },
-            function (callback) {
-                bookCreate(
-                    "The Wise Man's Fear (The Kingkiller Chronicle, #2)",
-                    'Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.',
-                    '9788401352836',
-                    authors[0],
-                    [genres[0]],
-                    callback,
-                );
-            },
-            function (callback) {
-                bookCreate(
-                    'The Slow Regard of Silent Things (Kingkiller Chronicle)',
-                    'Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.',
-                    '9780756411336',
-                    authors[0],
-                    [genres[0]],
-                    callback,
-                );
-            },
-            function (callback) {
-                bookCreate(
-                    'Apes and Angels',
-                    'Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...',
-                    '9780765379528',
-                    authors[1],
-                    [genres[1]],
-                    callback,
-                );
-            },
-            function (callback) {
-                bookCreate(
-                    'Death Wave',
-                    "In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...",
-                    '9780765379504',
-                    authors[1],
-                    [genres[1]],
-                    callback,
-                );
-            },
-            function (callback) {
-                bookCreate(
-                    'Test Book 1',
-                    'Summary of test book 1',
-                    'ISBN111111',
-                    authors[4],
-                    [genres[0], genres[1]],
-                    callback,
-                );
-            },
-            function (callback) {
-                bookCreate(
-                    'Test Book 2',
-                    'Summary of test book 2',
-                    'ISBN222222',
-                    authors[4],
-                    false,
-                    callback,
-                );
+                laptopCreate('NwoL Laptop', description, categories[1], 1999, 27, callback);
             },
         ],
         // optional callback
@@ -581,8 +484,6 @@ async.series(
     function (err, results) {
         if (err) {
             console.log('FINAL ERR: ' + err);
-        } else {
-            console.log('BOOKInstances: ' + bookinstances);
         }
         // All done, disconnect from database
         mongoose.connection.close();
--- routes/catalog.js
+++ routes/catalog.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+// GET HOME CATALOG WHICH SHOWS ALL CATEGORIES
+router.get('/', function (req, res, next) {
+    res.send('catalog with categories goes here');
+});
+
+// COMPUTER ACCESSORIES CATEGORY
+router.get('/computer-accessories', function (req, res, next) {
+    res.send('list of all comp acc products here');
+});
+
+//
+router.get('/computer-accessories/:id', function (req, res, next) {
+    res.send(`show detail page of the item ${req.params.id}`);
+});
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/electronic-devices', function (req, res, next) {
+    res.send('list of all electronic devices here');
+});
+/* GET users listing. */
+router.get('/phones', function (req, res, next) {
+    res.send('list of all phones products');
 });
 
 module.exports = router;
