cf668baca47a9de22e62a63308d48f866ee09796
Julio Bermudez <jbermudezfc90@gmail.com>
2020-05-22 10:57:28 -0400

    redesedesign categories page

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -4,6 +4,7 @@ exports.category_list = function (req, res, next) {
     Category.find()
         .sort([['name', 'ascending']])
         .then((categories) => {
+            console.log(categories);
             res.render('categories', {
                 title: 'Categories',
                 category_list: categories,
--- models/category.js
+++ models/category.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
     name: { type: String, minlength: 1, required: true },
     description: String,
+    file_url: String,
 });
 
 categorySchema.virtual('url').get(function () {
